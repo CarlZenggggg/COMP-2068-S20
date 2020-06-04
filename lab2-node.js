6d2e16fc9ac95828eb356c2fad691baba0f5f33d
CarlZenggggg <200402235@student.georgianc.on.ca>
2020-06-04 10:19:22 +0800

    made some minor changes

---
+++ lab2-node.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+
+app.get('/', (Request, Response) => {
+    https.get('https://insult.mattbas.org/api/insult', resp =>{ 
+        resp.on('data', chunk => {
+            Response.send(`
+                <http>
+                    <head><title>Oh Snap</title></head>
+
+                    <body>
+                        <h1>OH SNAP!</h1>
+                        <p style="font-size: 24px; color: green;">
+                            ${chunk.toString()}!
+                        </p>
+                    </body>
+                </http>
+            `);
+        });
+    });
+});
+
+app.listen(3000);
